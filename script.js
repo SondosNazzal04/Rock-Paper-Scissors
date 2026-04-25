139d30dd38689eb7362f7e50096ee6dcc8a584da
Sondos Nazzal <sondos.nazzal@outlook.com>
2026-04-25 17:03:15 +0300

    making it into rounds and game

--- script.js
+++ script.js
@@ -1,38 +1,71 @@
+const choices = Object.freeze({
+	"rock" : 0,
+	"paper" : 1,
+	"scissors" : 2
+});
+
 function getComputerChoice() {
 	const choice = Math.floor(Math.random() * 3);
 	return choice;
 }
-
-const choices = Object.freeze({
-	"Rock" : 0,
-	"Paper" : 1,
-	"Scissors" : 2
-});
 
 function getHumanChoice() {
 	const choice = prompt("Rock-Paper-Scissors!!!");
 	return choice;
 }
 
-const humanChoice = getHumanChoice();
-const computeChoice = getComputerChoice();
 
 // rock < paper < scissors < rock
 // 0 < 1 < 2 < 0
-if (choices[humanChoice] == computeChoice)
-	console.log("Tie");
-else if (choices[humanChoice] - 1 == computeChoice || (choices[humanChoice]) == 0 && computeChoice == 2)
-	console.log("You Win!!");
-else
-	console.log("You Lose :(");
+const humanChoice = getHumanChoice().toLowerCase();
+const computerChoice = getComputerChoice();
+
+function playGame() {
+	let humanScore = 0;
+	let computerScore = 0;
+
+	function playRound(human, computer) {
+		let winner;
+		let loser;
+		if (choices[human] == computer)
+		{
+			console.log("Tie");
+			return ;
+		}
+		else if (choices[human] - 1 == computer || (choices[human]) == 0 && computer == 2)
+		{
+			humanScore++;
+			winner = human;
+			loser = Object.keys(choices)[computer];
+			console.log("You Win!! ");
+		}
+		else
+		{
+			computerScore++;
+			winner = Object.keys(choices)[computer];
+			loser = human;
+			console.log("You Lose :( ");
+		}
+		console.log(winner + " beats " + loser);
+	}
+	for (let i = 0; i < 5; i++)
+		playRound(humanChoice, computerChoice);
+	if (humanScore > computerScore)
+		console.log("You win the game!");
+	else
+		console.log("You Lost the game!");
+}
+
+playGame(humanChoice, computerChoice);
 
 console.log("Human: " + humanChoice);
-if (computeChoice == 0)
-	console.log("Computer: Rock");
-else if (computeChoice == 1)
-	console.log("Computer: Paper");
-else
-	console.log("Computer: Scissors");
+console.log("Computer:" + Object.keys(choices)[computerChoice]);
+// if (computerChoice == 0)
+// 	console.log("Computer: Rock");
+// else if (computerChoice == 1)
+// 	console.log("Computer: Paper");
+// else
+// 	console.log("Computer: Scissors");
 // console.log(computeChoice);
 // console.log(getHumanChoice());
 // console.log(choices[getHumanChoice()]);
